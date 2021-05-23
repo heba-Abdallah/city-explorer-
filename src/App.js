546f97c6c70d0703308779234d0f1ea307ec5959
heba-Abdallah <heba.essam.m@hotmail.com>
2021-05-23 15:41:57 +0300

    adding a react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+
+  render() {
+    return (
+    <>
+
+        <h1>City Explorer</h1>
+        <form>
+          <input type='text' placeholder='add a city' />
+          <input type='submit' value='Explore!' />
+        </form>
+
+  </>
+    )
 }
-
+}
 export default App;
