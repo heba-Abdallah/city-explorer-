3dc6eb850c21eb1eb420dcce7575fab5db421109
heba-Abdallah <heba.essam.m@hotmail.com>
2021-05-30 00:01:01 +0300

    done with lab9-10

--- src/App.js
+++ src/App.js
@@ -1,136 +1,194 @@
 import React from 'react';
-import axios from 'axios';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Alert from 'react-bootstrap/Alert';
+// import axios from 'axios';
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+// import Alert from 'react-bootstrap/Alert';
+import Informationform from './components/forminf';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-let serverRoute = process.env.REACT_APP_SERVER;
-console.log('hi',serverRoute);
+import Cardsinfo from './components/cardsinfo';
+import WeatherCard from './components/weather';
+import Movie from './components/movie';
+import './App.css';
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
+      data2: '',
+      display: false,
+      show: false,
+      weatherArr: [],
+      rend: false,
+      newmoive: [],
 
-      searchQuery: '',
-      locData: '',
-      displayMap: false,
-      errorMessage: false,
-      cityInfo: {},
-      weatherData: [],
-      showWeather: false,
     }
   }
-  getLocation = async (e) => {
-    e.preventDefault();
-
-
-
-    let locUrl = `https://eu1.locationiq.com/v1/search.php?key=pk.8a6d5abe582c530444a1a198f0341145&q=${this.state.searchQuery}&format=json`
-
-    try {
-      let locResult = await axios.get(locUrl);
-      // console.log('hi', locResult.data);
-
-
-      for (let i = 0; i < locResult.data.length; i++) {
-        this.setState({
-          locData: locResult.data[i],
-          displayMap: true,
-        })
-        // console.log('i', locResult.data[i]);
-
-      }
-      // console.log('hello', this.state.locData);
-    }
-    catch {
-      this.setState({
-        displayMap: false,
-        errorMessage: true,
-      })
-    }
-    console.log('serverRoute', serverRoute);
-    try {
-      const url = `${serverRoute}/getLocation?city_name=${this.state.searchQuery}`;
-      // const url = `http://localhost:3001/getLocation?city_name=Amman`;
-      let infoData = await axios.get(url);
-      console.log('url', url);
-      console.log('info', infoData.data);
-
-
-      // console.log('info',weatherData);
-
-      this.setState({
-        weatherData: infoData.data,
-        showWeather: true,
-
-
-      })
-
-    }
-    catch (error) {
-      this.setState({
-        // weatherData: error.response,
-        showWeather: false,
-      })
-      console.log('sljdfhku')
-    }
-  }
-
-  updateSearchQuery = (event) => {
+  setmovie = (movie) => {
     this.setState({
-      searchQuery: event.target.value
+      newmoive: movie,
     })
-    // console.log('heba', this.state.searchQuery);
+    console.log('adfsa',this.state.newmoive);
   }
 
+
+
+  setData = (data1, showing) => {
+    this.setState({
+      data2: data1,
+      display: showing,
+      show: true,
+    })
+  }
+
+  setWeatherArr = (weatherData, showing) => {
+    this.setState({
+      weatherArr: weatherData,
+      rend: showing,
+    })
+  }
   render() {
     return (
-      <>
-
-        <h1>City Explorer</h1>
-        <Form onSubmit={this.getLocation}>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Add a City</Form.Label>
-            <Form.Control type="text" placeholder="Add a City" onChange={this.updateSearchQuery} />
-
-          </Form.Group>
-
-          <Button variant="primary" type="submit">
-            Explore!
-           </Button>
-        </Form>
-
-
-        {this.state.displayMap &&
-
-          <Card border="info" style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=pk.8a6d5abe582c530444a1a198f0341145&center=${this.state.locData.lat},${this.state.locData.lon}&zoom=<zoom>&size=<width>x<height>&format=<format>&maptype=<MapType>&markers=icon:<icon>|<latitude>,<longitude>`} alt='' />
-            <Card.Body>
-              <Card.Title>{this.state.locData.display_name}</Card.Title>
-            </Card.Body>
-          </Card>
-
+      <div>
+        <Informationform setData={this.setData} setWeather={this.setWeatherArr} setmovie={this.setmovie} />
+        {this.state.show &&
+          <Cardsinfo data2={this.state.data2} display={this.state.display} />
         }
-        {this.state.errorMessage &&
-
-          <Alert variant="success">
-            <p>
-              error in getting the data
-          </p>
-          </Alert>
-        }
-        { this.state.weatherData.map((item, idx) => {
-          return <p key={idx}>{item.date} and {item.description}</p>
-        })}
-        <p>{this.state.item}</p>
-      </>
+        {this.state.show === true &&
+          <WeatherCard display={this.state.rend} weatherData={this.state.weatherArr} />}
+        {this.state.show === true &&
+          <Movie display={this.state.rend} setmovie={this.state.newmoive} />}
+      </div>
     )
   }
-
 }
 export default App;
+
+/////////////////////
+// let serverRoute = process.env.REACT_APP_SERVER;
+// console.log('hi',serverRoute);
+
+// class App extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+
+//       searchQuery: '',
+//       locData: '',
+//       displayMap: false,
+//       errorMessage: false,
+//       cityInfo: {},
+//       weatherData: [],
+//       showWeather: false,
+//     }
+//   }
+//   getLocation = async (e) => {
+//     e.preventDefault();
+
+
+
+//     let locUrl = `https://eu1.locationiq.com/v1/search.php?key=pk.8a6d5abe582c530444a1a198f0341145&q=${this.state.searchQuery}&format=json`
+
+//     try {
+//       let locResult = await axios.get(locUrl);
+//       // console.log('hi', locResult.data);
+
+
+//       for (let i = 0; i < locResult.data.length; i++) {
+//         this.setState({
+//           locData: locResult.data[i],
+//           displayMap: true,
+//         })
+//         // console.log('i', locResult.data[i]);
+
+//       }
+//       // console.log('hello', this.state.locData);
+//     }
+//     catch {
+//       this.setState({
+//         displayMap: false,
+//         errorMessage: true,
+//       })
+//     }
+//     console.log('serverRoute', serverRoute);
+//     try {
+//       const url = `${serverRoute}/getLocation?city_name=${this.state.searchQuery}`;
+//       // const url = `http://localhost:3001/getLocation?city_name=Amman`;
+//       let infoData = await axios.get(url);
+//       console.log('url', url);
+//       console.log('info', infoData.data);
+
+
+//       // console.log('info',weatherData);
+
+//       this.setState({
+//         weatherData: infoData.data,
+//         showWeather: true,
+
+
+//       })
+
+//     }
+//     catch (error) {
+//       this.setState({
+//         // weatherData: error.response,
+//         showWeather: false,
+//       })
+//       console.log('sljdfhku')
+//     }
+//   }
+
+//   updateSearchQuery = (event) => {
+//     this.setState({
+//       searchQuery: event.target.value
+//     })
+//     // console.log('heba', this.state.searchQuery);
+//   }
+
+//   render() {
+//     return (
+//       <>
+
+//         <h1>City Explorer</h1>
+//         <Form onSubmit={this.getLocation}>
+//           <Form.Group controlId="formBasicEmail">
+//             <Form.Label>Add a City</Form.Label>
+//             <Form.Control type="text" placeholder="Add a City" onChange={this.updateSearchQuery} />
+
+//           </Form.Group>
+
+//           <Button variant="primary" type="submit">
+//             Explore!
+//            </Button>
+//         </Form>
+
+
+//         {this.state.displayMap &&
+
+//           <Card border="info" style={{ width: '18rem' }}>
+//             <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=pk.8a6d5abe582c530444a1a198f0341145&center=${this.state.locData.lat},${this.state.locData.lon}&zoom=<zoom>&size=<width>x<height>&format=<format>&maptype=<MapType>&markers=icon:<icon>|<latitude>,<longitude>`} alt='' />
+//             <Card.Body>
+//               <Card.Title>{this.state.locData.display_name}</Card.Title>
+//             </Card.Body>
+//           </Card>
+
+//         }
+//         {this.state.errorMessage &&
+
+//           <Alert variant="success">
+//             <p>
+//               error in getting the data
+//           </p>
+//           </Alert>
+//         }
+//         { this.state.weatherData.map((item, idx) => {
+//           return <p key={idx}>{item.date} and {item.description}</p>
+//         })}
+//         <p>{this.state.item}</p>
+//       </>
+//     )
+//   }
+
+// }
+// export default App;
---
+++ src/components/weather.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+
+class WeatherCard extends React.Component {
+
+    //  constructor(props){
+    //      super(props);
+    //      this.state={
+    //          weatherArr:this.props.weatherData,
+    //      }
+    //  }
+    render() {
+
+
+        return (
+            <>
+                {this.props.display && <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>
+                            Weather Data
+                        </Card.Title>
+                        <Card.Text>
+                            {/* {console.log(this.state.weatherData)}  */}
+                            Date: {this.props.weatherData[0].date}<br />
+                            Description: {this.props.weatherData[0].description}<br />
+                            Date: {this.props.weatherData[1].date}<br />
+                            Description: {this.props.weatherData[1].description}<br />
+                            Date: {this.props.weatherData[2].date}<br />
+                            Description: {this.props.weatherData[2].description}<br />
+
+                        </Card.Text>
+                    </Card.Body>
+                </Card>}
+                {this.props.display === false && <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>
+                            Weather Data with error: {this.props.weatherData.status}
+                        </Card.Title>
+                        <Card.Text>
+                            {this.props.weatherData.data}
+
+
+                        </Card.Text>
+                    </Card.Body>
+                </Card>}
+
+            </>
+        )
+    }
+}
+
+export default WeatherCard;
